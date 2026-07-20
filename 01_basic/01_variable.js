50909f3b07b7e067bc749514e57ce8fca8a443f6
vishalchandra86 <vishal.chandra09@gmail.com>
2026-07-20 04:28:00 +0000

    variables done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,9 @@
+const accountId=21211;
+let accountEmail="abc@gmail.com";
+var accountPassword="123456";
+accountCity="Mumbai";
+
+//accountId=434222;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
